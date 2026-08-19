443ea9a6c0d26a6248268faab2831a32a9a7889f
vicky kumar <vk3354978@gmail.com>
2026-08-20 01:19:15 +0530

    Throw is done

--- src/ThrowDemo1.java
+++ src/ThrowDemo1.java
@@ -1,4 +1,4 @@
-public class ThrowDemo {
+public class ThrowDemo1 {
     static void main() {
         int age=14;
         validAge(age);
@@ -9,7 +9,7 @@ public class ThrowDemo {
         {
             //System.out.println(10/0);
             throw new IllegalArgumentException("age is invalid ");
-            //object created by jvm
+            //object created by jvm...
             //handover to default Exception
             //but in the case of throw we can create own object using throw
         }
