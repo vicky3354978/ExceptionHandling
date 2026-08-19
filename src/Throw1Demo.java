a91c2f10c62dcbaca400b8fdd4b748f3ff8abf87
vicky kumar <vk3354978@gmail.com>
2026-08-20 01:21:48 +0530

    Throw is done

---
+++ src/Throw1Demo.java
@@ -0,0 +1,20 @@
+public class Throw1Demo {
+        static void main() {
+            int age=14;
+            validAge(age);
+        }
+        public static void validAge(int age)
+        {
+            if(age<18)
+            {
+                //System.out.println(10/0);
+                throw new IllegalArgumentException("age is invalid ");
+                //object created by jvm...
+                //handover to default Exception
+                //but in the case of throw we can create own object using throw
+            }
+            System.out.println("age is approved :");
+        }
+    }
+
+
